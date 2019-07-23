a58c2fc08cef01b9406c590ab5ac9e258422f3d7
Allabergen <allromis@gmail.com>
2019-07-23 19:49:26 +0600

    Start Shopd App

---
+++ lib/models/product.dart
@@ -0,0 +1,9 @@
+class Product {
+  String id;
+  String title;
+  String description;
+  double price;
+  String imageUrl;
+
+  Product({this.id, this.title, this.description, this.price, this.imageUrl});
+}
